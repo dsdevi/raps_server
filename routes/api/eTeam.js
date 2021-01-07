62e4328430a2bf7f9070eaa94dac04b0b8d25a4b
Devi-Laptop <desilvadevishke@gmail.com>
2021-01-07 21:15:45 +0530

    added availability toggle

--- routes/api/eTeam.js
+++ routes/api/eTeam.js
@@ -1,6 +1,36 @@
 const router = require("express").Router();
 let ETeam = require("../../models/eTeam.model");
-let ETeamSession = require('../../models/eTeamSession.model');
+let ETeamSession = require("../../models/eTeamSession.model");
+
+router.route("/toggleavailability").post((req, res) => {
+  const username = req.body.username;
+
+  if (!username || username.length < 4) {
+    return res.send({
+      success: false,
+      message: "Error: Username invalid.",
+    });
+  }
+
+  ETeam.findOne({ username: username })
+    .then((object) => {
+      let availability = !object.availability;
+      console.log(availability);
+      ETeam.findOneAndUpdate(
+        { username: username },
+        { availability: availability }
+      ).then(() => {
+        return res.send({
+          success: true,
+          availability: availability,
+          message: "availability updated",
+        });
+      });
+    })
+    .catch((err) => {
+      return res.send({ success: false, message: "User not found" });
+    });
+});
 
 router.route("/list").get((req, res) => {
   ETeam.find()
@@ -9,96 +39,108 @@ router.route("/list").get((req, res) => {
 });
 
 //Sign in
-router.route('/signin').post((req, res) => {
+router.route("/signin").post((req, res) => {
   const { body } = req;
-  const {username, password} = body;
-  
-  if(!username || username.length<4){
-      return res.send({
-          success:false,
-          message:'Error: Username invalid.'
-      })}
+  const { username, password } = body;
 
-  if(!password|| password.length<4){
-      return res.send({
-          success:false,
-          message:'Error: Password invalid.'
-      })}    
+  if (!username || username.length < 4) {
+    return res.send({
+      success: false,
+      message: "Error: Username invalid.",
+    });
+  }
+
+  if (!password || password.length < 4) {
+    return res.send({
+      success: false,
+      message: "Error: Password invalid.",
+    });
+  }
   //find by username
-  ETeam.find({
-      username:username
-  },(err,users)=>{
-      if(err){
-          return res.send({
-              success:false,
-              message:'Error:Server error'
-          })
+  ETeam.find(
+    {
+      username: username,
+    },
+    (err, users) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: "Error:Server error",
+        });
       }
-      if(users.length!=1){
-          return res.send({
-              success:false,
-              message:'Error:Invalid username (password validation)'
-          })
+      if (users.length != 1) {
+        return res.send({
+          success: false,
+          message: "Error:Invalid username (password validation)",
+        });
       }
       const eTeam = users[0];
-      if(!eTeam.validPassword(password)){
-          return res.send({
-              success:false,
-              message:'Error:Invalid password'
-          })
+      if (!eTeam.validPassword(password)) {
+        return res.send({
+          success: false,
+          message: "Error:Invalid password",
+        });
       }
-      if(eTeam.isDeleted){
-          return res.send({
-              success:false,
-              message:'Error:Deleted account'
-          })
+      if (eTeam.isDeleted) {
+        return res.send({
+          success: false,
+          message: "Error:Deleted account",
+        });
       }
       //otherwise create user session
       const eTeamSession = new ETeamSession();
-      eTeamSession.username=eTeam.username;
-      eTeamSession.save((err,doc)=>{
-          if(err){
-              return res.send({
-                  success:false,
-                  message:'Error:Server error',
-              });
-          };
-
+      eTeamSession.username = eTeam.username;
+      eTeamSession.save((err, doc) => {
+        if (err) {
           return res.send({
-              success:true,
-              message:'Valid sign in',
-              token:doc._id //session id taken from mongodb doc (record)
+            success: false,
+            message: "Error:Server error",
           });
+        }
+
+        return res.send({
+          success: true,
+          message: "Valid sign in",
+          token: doc._id, //session id taken from mongodb doc (record)
+          contactNumber: eTeam.contactNumber,
+          availability: eTeam.availability,
+          lat: eTeam.lat,
+          lng: eTeam.lng,
+          name: eTeam.name,
+        });
       });
-      
-  });
+    }
+  );
 });
 
 //Logout
-router.route('/logout').get((req, res) => {
+router.route("/logout").get((req, res) => {
   //get the token
-  const { query } =req;
+  const { query } = req;
   const { token } = query;
   //verify if its one of a kind and not deleted.
-  ETeamSession.findOneAndUpdate({   
-          _id:token, 
-          isDeleted:false
-      },{
-          $set:{isDeleted:true}
-      }, null,(err,sessions) =>{
-          if(err){
-              return res.send({
-                  success:false,
-                  message:'Error:Server error or Session not found'
-              })
-          }
-          return res.send({
-              success:true,
-              message:'Session deleted'
-          })
-})
-})
-
-
+  ETeamSession.findOneAndUpdate(
+    {
+      _id: token,
+      isDeleted: false,
+    },
+    {
+      $set: { isDeleted: true },
+    },
+    null,
+    (err, sessions) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: "Error:Server error or Session not found",
+        });
+      }
+      return res.send({
+        success: true,
+        message: "Session deleted",
+      });
+    }
+  );
+});
 
 module.exports = router;
